3db260e76eeb7728b15efba87e812137054b43f6
repl.it user <replituser@example.com>
2020-10-26 14:37:45 +0000

    cleaned up code and fixed for loop

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -1,16 +1,19 @@
 class Zahlenfilter {
-  
+
+  public Zahlenfilter() {
+  }
+
   public void writeNumbers() {
-    for (int i = 0; i < 200; i++) {
+    for (int i = 1; i <= 200; i++) {
       if (i % 5 == 0) {
         System.out.println(i + " ist durch 5 teilbar!");
-      }  
-      if (i % 10 == 9) {
-        System.out.println(i + " endet auf 9   ! ");
       }
-      int sum = i + i -1;
+      if (i % 10 == 9) {
+        System.out.println(i + " endet auf 9!");
+      }
+      int sum = i + i - 1;
       if (sum % 3 == 0) {
-        System.out.println(i + "  und " + (i-1) + " addiert ergeben " + sum + " und " + sum + " ist durch 3 teilbar");
+        System.out.println(i + "  und " + (i - 1) + " addiert ergeben " + sum + " und " + sum + " ist durch 3 teilbar!");
       }
     }
   }
